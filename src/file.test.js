788643b2421e4b413eea5c989085a722f5719a82
Leonid Shagabutdinov <leonid@shagabutdinov.com>
2021-08-09 14:27:26 +0300

    [newfunc-with-test] add spec for new func

--- src/file.test.js
+++ src/file.test.js
@@ -1,7 +1,17 @@
-const { someFunc } = require('./file')
+const { someFunc, newOtherFunc } = require('./file')
 
 describe("test 1", () => {
   it("test 2", async () => {
     expect(someFunc(1)).toEqual(1);
+  });
+});
+
+describe(newOtherFunc, () => {
+  it("test 1", async () => {
+    expect(newOtherFunc(10)).toEqual(100);
+  });
+
+  it("test 2", async () => {
+    expect(newOtherFunc(1)).toEqual(1000);
   });
 });
